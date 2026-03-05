78edf6ae928ce8e0d19047273276ac6efe5468aa
who-biz <37732338+who-biz@users.noreply.github.com>
2026-03-05 16:59:58 -0500

    Remove DecryptParams, we don't use it anymore

--- model-lib/src/main/java/cash/z/ecc/android/sdk/model/DecryptParams.kt
+++
@@ -1,9 +0,0 @@
-package cash.z.ecc.android.sdk.model
-
-
-data class DecryptParams(
-    val ivkBytes: ByteArray?,
-    val ephemeralPublicKey: ByteArray,
-    val encryptedData: ByteArray,
-    val symmetricKeyBytes: ByteArray?
-)
